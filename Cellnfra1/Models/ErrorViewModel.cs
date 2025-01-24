984126a8aa19a7b6e5af06bae1c7f3fc8bd32912
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2025-01-24 21:29:24 +0530

    Add project files.

---
+++ Cellnfra1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cellnfra1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
